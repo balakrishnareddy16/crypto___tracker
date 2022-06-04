a84953d1bdb0b51a4236a2f427431ca2b6cda528
Bala Krishna Reddy <97590110+balakrishnareddy16@users.noreply.github.com>
2022-06-04 23:38:04 +0530

    Update Header.js

--- crypto-tracker/src/components/Header.js
+++ crypto-tracker/src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              KITS CRYPTO
+              CRYPTO
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
